86cb305af132e44f8c38f4ea061e04b4b4ced785
Simba <sireri@yahoo.com>
2017-05-23 15:00:38 +0300

    Adding STATIC_ROOT

--- localib/settings.py
+++ localib/settings.py
@@ -101,7 +101,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
