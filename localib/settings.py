ecc9373c08e67c3b148bafdff5d4596acc13de23
StevenSimba <sireri@yahoo.com>
2017-05-24 10:43:18 +0300

    setup issues

--- localib/settings.py
+++ localib/settings.py
@@ -104,7 +104,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) 
 # Simplified static file serving
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
